fc84833a7f9716098fe6b4d7e9045e1acff66692
latruonghai@gmail.com <latruonghai@gmail.com>
2021-06-04 19:46:07 +0700

    Update core: using static data

---
+++ app/backend/core/Paths.py
@@ -0,0 +1,9 @@
+
+file_path = [
+    './app/backend/model/OkapiBM25/src/weight of Dataset/avgdl.pkl',
+    './app/backend/model/OkapiBM25/src/weight of Dataset/dl.pkl',
+    './app/backend/model/OkapiBM25/src/weight of Dataset/dltable.pkl',
+    './app/backend/model/OkapiBM25/src/weight of Dataset/file2terms.pkl',
+    './app/backend/model/OkapiBM25/src/weight of Dataset/files.pkl',
+    './app/backend/model/OkapiBM25/src/weight of Dataset/idf.pkl',
+    './app/backend/model/OkapiBM25/src/weight of Dataset/invertedIndex.pkl']
