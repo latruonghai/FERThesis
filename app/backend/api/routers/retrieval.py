2a01b92949a96b7bd4f5a3a03ae0be41e505f742
latruonghai@gmail.com <latruonghai@gmail.com>
2021-06-04 19:54:07 +0700

    Update files

--- app/backend/api/routers/retrieval.py
+++ app/backend/api/routers/retrieval.py
@@ -9,15 +9,17 @@ from app.backend.model import get_db
 router = APIRouter()
 
 
-
 @router.get('/IR/search', response_class=HTMLResponse)
-async def get_retrieve(query, requests: Request, request=Tourism.TourismInRequest, db: Session = Depends(get_db)):
+async def get_retrieve(query, requests: Request, 
+                        request=Tourism.TourismInRequest, 
+                        db: Session = Depends(get_db)):
     return retrieval.get_retrieval(
         query=query,
         request=request,
         requests=requests,
         db=db)
-    
+
+
 @router.get('/IR', response_class=HTMLResponse)
-async def render(request:Request, db: Session=Depends(get_db)):
+async def render(request: Request, db: Session = Depends(get_db)):
     return retrieval.render_all(request, db)
--- app/backend/app.py
+++ app/backend/app.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from .api.routers import api
+from .model.base import models
+from .model import engine
 
 
 def generate_app():
     app = FastAPI()
-    app.mount('/static', StaticFiles(directory="app/backend/jinja/static"), name="static")
+
+    models.Base.metadata.create_all(engine)
+    app.mount(
+        '/static',
+        StaticFiles(
+            directory="app/backend/jinja/static"),
+        name="static")
     app.include_router(api.router)
     return app
 
