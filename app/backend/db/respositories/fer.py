d89cbcf1f2c060e4ffe75526488eb86379478372
latruonghai@gmail.com <latruonghai@gmail.com>
2021-11-09 15:14:45 +0700

    Update fer.py in respositories folder

--- app/backend/db/respositories/fer.py
+++ app/backend/db/respositories/fer.py
@@ -1,8 +1,14 @@
 from app.backend.configs import config_model
 from app.backend.utils.module_recognize.fer import FER
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
 
 fer = FER(config_model)
 
 
 def detect_face_with_image(img, gpu, show):
-    return fer.detect_emotion_with_image(img, gpu, show=show)
+    return fer.detect_emotion_with_image(img, gpu=gpu, show=show)
