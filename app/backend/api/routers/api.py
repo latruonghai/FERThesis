ee896b5834d50616f7a079807771583e9c586cb8
latruonghai@gmail.com <latruonghai@gmail.com>
2021-12-23 19:03:25 +0700

    Update router 23/12/2021

--- app/backend/api/routers/api.py
+++ app/backend/api/routers/api.py
@@ -6,8 +6,15 @@ router.include_router(fer.router, tags=["Recognition"])
 # router.include_router(images.router, tags=["Images"])
 # router.include_router(retrieval.router, tags=["IR"])
 #router.include_router(hellman.router, tags=["hellman"])
+@router.post("/")
+async def hell(text: str):
+    return {
+        "title": "Hello",
+        "text": text
+    }
 
 @router.get("/")
 async def hello_world():
     return {"title": "Hello",
             "body": "What's up"}
+
--- app/backend/api/routers/fer.py
+++ app/backend/api/routers/fer.py
@@ -1,39 +1,22 @@
 from fastapi import APIRouter, File, UploadFile
 from starlette.responses import StreamingResponse
+from fastapi.responses import JSONResponse
 from app.backend.db.respositories.fer import detect_face_with_image
-from app.backend.utils.module_recognize.fer import read_image_from_byte
+from app.backend.utils.module_recognize.fer import read_image_from_byte, decode_image, encode_image
+from app.backend.model.schemas.FER import FERRequest
 # from app.backend.model.schemas.FER import
 import numpy as np
 from PIL import Image
-from cv2 import cvtColor, COLOR_BGR2RGB, COLOR_RGB2BGR, imencode
+
 import base64
 import io
 
 router = APIRouter()
 
 
-@router.post("/fer/recognition/image")
+@router.post("/fer/recognition/image", response_class=JSONResponse)
 # def recog
-async def api_recognize(file: UploadFile = File(...), gpu: int = 1, show: bool = False):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+async def api_recognize(file: FERRequest):
+    return detect_face_with_image(file)
+    
 
-    if not extension:
-        return "Image must be jpg or png format"
-    pic = read_image_from_byte(await file.read())
-    image = np.array(pic)
-    image = cvtColor(image, COLOR_RGB2BGR)
-    # print(image.shape)
-    img, label = detect_face_with_image(image, gpu=int(gpu), show=bool(show))
-    # img = cvtColor(img, COLOR_BGR2RGB)
-    # print(img.shape)
-    # im_pil = Image.fromarray(img)
-    # encode = base64.b64decode(img)
-
-    res, im_png = imencode(".png", img)
-    return StreamingResponse(
-        io.BytesIO(
-            im_png.tobytes()),
-        media_type="image/png")
-    # return im_pil
-
-    # print(img)
