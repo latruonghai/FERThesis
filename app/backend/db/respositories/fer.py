7a8a377706aa0d20e30946bc06f573fcf8ee6b44
latruonghai@gmail.com <latruonghai@gmail.com>
2021-12-23 19:02:16 +0700

    Update repositories folder including fer 23/12/2021

--- app/backend/db/respositories/fer.py
+++ app/backend/db/respositories/fer.py
@@ -1,7 +1,15 @@
+from fastapi.responses import JSONResponse
 from app.backend.configs import config_model
+
 from app.backend.utils.module_recognize.fer import FER
 from tensorflow.compat.v1 import ConfigProto
+from app.backend.model.schemas.FER import FERRequest
 from tensorflow.compat.v1 import InteractiveSession
+
+from app.backend.utils.module_recognize.fer import read_image_from_byte, decode_image, encode_image
+# from app.backend.model.schemas.FER import FERRequest
+# from app.backend.model.schemas.FER import
+
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
@@ -10,5 +18,18 @@ session = InteractiveSession(config=config)
 fer = FER(config_model)
 
 
-def detect_face_with_image(img, gpu, show):
-    return fer.detect_emotion_with_image(img, gpu=gpu, show=show)
+def detect_face_with_image(file: FERRequest):
+    
+    image = decode_image(file.file)
+    
+    img, dic_face = fer.detect_emotion_with_image(image, gpu=int(file.gpu), show=bool(file.show))
+    # print(dic_face)
+    imgcode = encode_image(img)
+
+    return JSONResponse(
+        {
+            "header": "Content-type: application/json",
+            "body": {"state": 1 if dic_face else 0 ,"image":str(imgcode), "dict_face": dic_face}
+        }
+    )
+    
